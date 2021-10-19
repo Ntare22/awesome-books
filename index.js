e8af6e94d7a3c680851c0896cff64fa7bc2f7cf8
Lino09 <onosmx@gmail.com>
2021-10-19 17:23:03 -0500

    Add linter fixes

--- index.js
+++ index.js
@@ -1,54 +1,23 @@
-const bookStand = document.querySelector('#books')
-class Book {
-  constructor(title, author, id) {
-    this.title = title;
-    this.author = author;
-    this.id = id;
-  }
-}
-
-class BookShelf {
-  constructor(books = []) {
-    this.books = books
-  }
-  set addBook(uniqueId) {
-    const newBook = new Book(document.querySelector('#title').value, document.querySelector('#title').value, uniqueId)
-    this.books.unshift(newBook)
-    this.preserveData()
-    fillBookShelf()
-  };
-  set removeBook(book) {
-    this.books = this.books.filter((el) => el.id !== book.id)
-    this.preserveData()
-    fillBookShelf()
-  }
-  preserveData() {
-    let data = JSON.stringify(this.books)
-    window.localStorage.setItem('data', data)
-  }
-}
-
-let bookShelf = new BookShelf()
-document.querySelector('#add-book').addEventListener('submit', (event) => {
-  event.preventDefault()
-  const uniqueId = document.querySelector('#title').value + Math.floor(Math.random() * 1000)
-  bookShelf.addBook = uniqueId
-})
+const bookStand = document.querySelector('#books');
 
 function fillBookShelf() {
   bookStand.innerHTML = '';
   if (window.localStorage.getItem('data')) {
     const storage = window.localStorage.getItem('data');
+    // eslint-disable-next-line no-use-before-define
     bookShelf.books = JSON.parse(storage);
   }
+  // eslint-disable-next-line no-use-before-define
   bookShelf.books.forEach((book) => {
     const bookContainer = document.createElement('div');
     bookContainer.classList = 'card';
     const bookTitle = document.createElement('h3');
     const bookAuthor = document.createElement('p');
     const removeBtn = document.createElement('button');
-    // eslint-disable-next-line no-use-before-define
-    removeBtn.addEventListener('click', () => bookShelf.removeBook = book);
+    removeBtn.addEventListener('click', () => {
+      // eslint-disable-next-line no-use-before-define
+      bookShelf.removeBook = book;
+    });
     bookTitle.innerHTML = book.title;
     bookAuthor.innerHTML = book.author;
     removeBtn.innerHTML = 'Remove';
@@ -59,5 +28,36 @@ function fillBookShelf() {
     bookStand.appendChild(bookContainer);
   });
 }
+
+class BookShelf {
+  constructor(books = []) {
+    this.books = books;
+  }
+
+  set addBook(uniqueId) {
+    const newBook = { title: document.querySelector('#title').value, author: document.querySelector('#title').value, id: uniqueId };
+    this.books.unshift(newBook);
+    this.preserveData();
+    fillBookShelf();
+  }
+
+  set removeBook(book) {
+    this.books = this.books.filter((el) => el.id !== book.id);
+    this.preserveData();
+    fillBookShelf();
+  }
+
+  preserveData() {
+    const data = JSON.stringify(this.books);
+    window.localStorage.setItem('data', data);
+  }
+}
+
+const bookShelf = new BookShelf();
+document.querySelector('#add-book').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const uniqueId = document.querySelector('#title').value + Math.floor(Math.random() * 1000);
+  bookShelf.addBook = uniqueId;
+});
 
 fillBookShelf();
