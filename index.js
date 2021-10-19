6760435cff14c28f21eddd2d18d20879cb718419
Lino09 <onosmx@gmail.com>
2021-10-19 14:38:46 -0500

    Add Book class to create and Add books

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-let books = [
-  {
-    id: 1,
-    title: 'Example book title',
-    author: 'Jim Ntare & Abel Herrera',
-  },
-];
+let books = [{
+  id: 1,
+  title: 'Example book title',
+  author: 'Jim Ntare & Abel Herrera',
+}, ];
+
+class Book {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+}
+
 
 function removeBook(item) {
   books = books.filter(book => book.id !== item.id);
@@ -15,40 +22,42 @@ function removeBook(item) {
 
 const booksContainer = document.getElementById('books');
 const addBookForm = document.querySelector('#add-book');
+
 function populateBooks() {
-  if(window.localStorage.getItem('bookShelf')){
-    let storage = window.localStorage.getItem('bookShelf') 
+  if (window.localStorage.getItem('bookShelf')) {
+    let storage = window.localStorage.getItem('bookShelf')
     books = JSON.parse(storage)
   }
   books.forEach((book) => {
     const bookContainer = document.createElement('div');
     bookContainer.classList = 'card';
-    
-  const bookTitle = document.createElement('h3');
-  const bookAuthor = document.createElement('p');
-  const removeBtn = document.createElement('button');
 
-  removeBtn.addEventListener('click', () => removeBook(book));
-  
-  bookTitle.innerHTML = book.title;
-  bookAuthor.innerHTML = book.author;
-  removeBtn.innerHTML = 'Remove';
-  
-  bookContainer.appendChild(bookTitle);
-  bookContainer.appendChild(bookAuthor);
-  bookContainer.appendChild(removeBtn);
-  booksContainer.appendChild(bookContainer);
-});
+    const bookTitle = document.createElement('h3');
+    const bookAuthor = document.createElement('p');
+    const removeBtn = document.createElement('button');
+
+    removeBtn.addEventListener('click', () => removeBook(book));
+
+    bookTitle.innerHTML = book.title;
+    bookAuthor.innerHTML = book.author;
+    removeBtn.innerHTML = 'Remove';
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(removeBtn);
+    booksContainer.appendChild(bookContainer);
+  });
 }
 populateBooks();
 
-addBookForm.addEventListener('submit',(event) => {
+addBookForm.addEventListener('submit', (event) => {
   event.preventDefault()
   let title = document.querySelector('#title').value;
   let author = document.querySelector('#author').value;
   if (title !== '' && author !== '') {
     const uniqueId = document.querySelector('#title').value + Math.floor(Math.random() * 1000)
-    books.push({id: uniqueId,title: document.querySelector('#title').value, author: document.querySelector('#author').value})
+    let newBook = new Book(document.querySelector('#title').value,document.querySelector('#author').value,uniqueId)
+    books.push(newBook)
     booksContainer.innerHTML = '';
     preserveBookShelf();
     populateBooks();
@@ -60,9 +69,8 @@ addBookForm.addEventListener('submit',(event) => {
 })
 
 let bookShelf = ''
+
 function preserveBookShelf() {
   bookShelf = JSON.stringify(books)
   window.localStorage.setItem('bookShelf', bookShelf)
 }
-
-
